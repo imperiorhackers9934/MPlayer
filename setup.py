14ea57c424689d9776fbe76afdb41a3724688789
Imperior Hackers <58589595+imperiorhackers9934@users.noreply.github.com>
2021-09-17 16:59:54 +0530

    Setup File

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+import sys
+os.system("pip install tkinter")
+os.system("pip install pygame")
+os.system("pip install PIL")
+os.system("pip install mutagen.mp3")
+print("")
+print("Your Setup has been Completed :)")
